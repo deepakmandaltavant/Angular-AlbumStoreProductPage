e9a71ee554dbdc8b35aeda02a890582ab87b285b
Deepak Mandal <deepak.mandal@tavant.com>
2019-04-11 12:34:50 +0530

    Completed the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import {ProductService} from "./product/product.service";
-
-const appRoutes: Routes = [
-  { path: 'albums', component: ProductDescriptionComponent },
-  { path: 'album',component: ProductPageComponent,data: { title: 'Heroes List' }},
-  { path: '',redirectTo: '/album',pathMatch: 'full'},
-  { path: '**', component: ProductDescriptionComponent }
- ];
 
 @NgModule({
   declarations: [
@@ -23,12 +14,11 @@ const appRoutes: Routes = [
     ProductDescriptionComponent
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
